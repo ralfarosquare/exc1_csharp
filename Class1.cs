e8d971a0ba09769559ebb6aa5518b18c06526ad3
ralfarosquare <90344277+ralfarosquare@users.noreply.github.com>
2021-09-23 17:38:04 -0500

    excercise 1

---
+++ Class1.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+    namespace Class2
+
+    {
+
+        class Program
+
+        {
+
+            static void Ma(string[] args)
+
+            {
+
+                //Print First Five Values
+
+                int[] integerList = { 9, 8, 7, 6, 5, 4, 3, 2 };
+
+
+
+                IEnumerable<int> firstFive = integerList.OrderByDescending(item => item).Take(5);
+
+
+
+                Console.WriteLine("The first five values are:");
+
+                foreach (int item in firstFive)
+
+                {
+
+                    Console.WriteLine(item);
+
+                }
+
+
+
+                //PrintSmallestNumber
+
+                int[] arrayList = new int[7] { 20, 400, 40, 50, 10, 1, 4000 };
+
+                int smallest = arrayList[0];
+
+                for (int i = 0; i < arrayList.Length; i++)
+
+                {
+
+                    if (arrayList[i] < smallest)
+
+                        smallest = arrayList[i];
+
+                }
+
+                Console.WriteLine("The smallest number of the array is:" + smallest);
+
+
+
+                //Print Dictionary 
+
+                Dictionary<String, int> myDictionary = new Dictionary<string, int>
+
+            {
+
+                { "Sunday", 1 },
+
+                { "Monday", 2 },
+
+                { "Tuesday", 3 },
+
+                { "Wednesday", 4 },
+
+                { "Thursday", 5 },
+
+                { "Friday", 6 },
+
+                { "Saturday", 6 }
+
+            };
+
+
+
+                foreach (KeyValuePair<String, int> entry in myDictionary)
+
+                {
+
+                    Console.WriteLine(entry.Key + " : " + entry.Value);
+
+                }
+
+            }
+
+
+
+
+
+
+
+        }
+
+    }
---
+++ Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+
+namespace exc1_csharp
+{
+    class Program
+
+    {
+
+        static void Main(string[] args)
+
+        {
+
+            // String  Comparison 
+
+            string firstName = "Rodrigo";
+
+            string lastName = "Alfaro";
+
+
+
+
+
+            if (firstName == lastName)
+
+            {
+
+                Console.WriteLine("Both strings are the same");
+
+            }
+
+            else
+
+            {
+
+                Console.WriteLine("Are different strings");
+
+            }
+
+
+
+            // Biggest Number
+
+            int[] array1 = new int[5] { 1, 3, 10, 7, 9 };
+
+            Array.Sort(array1);
+
+            Console.WriteLine("The biggest number is:  " + array1[4]);
+
+
+
+            //sort List
+
+            int[] sortarray = new int[6] { 100, 200, 500, 700, 900, 1000 };
+
+            Console.Write("List: ");
+
+            foreach (int i in sortarray)
+
+            {
+
+                Console.Write(i + " ");
+
+            }
+
+            Console.WriteLine();
+
+            Console.Write("Sorted List: ");
+
+            Array.Sort(sortarray);
+
+            foreach (int desc in sortarray)
+
+            {
+
+                Console.WriteLine(desc);
+
+            }
+
+
+
+            // Count String length
+
+
+
+            string name = "Unosquare";
+
+            int nameLength = name.Length;
+
+            Console.WriteLine("The name  of the string contains " + nameLength + " " + "letters");
+
+        }
+
+
+
+
+
+    }
+
+
+
+}
+
+
+
+
+
